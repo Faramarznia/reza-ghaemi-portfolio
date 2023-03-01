c50154fbceb77c7e48491e8567c1a47a06c53055
Faramarz Faramarznia <Faramarznia.developer@gmail.com>
2023-03-01 23:43:56 +0330

    target="_blank"

--- script.js
+++ script.js
@@ -166,15 +166,16 @@ recContainer.addEventListener("mousemove", (e) => {
   recContainer.scrollLeft = scrollLeft - walk;
 });
 
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+// const hamburger = document.querySelector(".hamburger")
+// const navMenu = document.querySelector(".nav-menu")
 
-hamburger.addEventListener("click", ()=> {
-  hamburger.classList.toggle("active")
-  navMenu.classList.toggle("active")
-})
+// hamburger.addEventListener("click", ()=> {
+//   hamburger.classList.toggle("active")
+//   navMenu.classList.toggle("active")
+// })
 
-document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
-  hamburger.classList.remove("active")
-  navMenu.classList.remove("active")
-}))
+// document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+//   hamburger.classList.remove("active")
+//   navMenu.classList.remove("active")
+// }))
+
